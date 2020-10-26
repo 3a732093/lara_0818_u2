81f09afd0193916a8791f24b2b771771edf5efef
廖韋臣 <rax02270521@gmail.com>
2020-10-26 21:24:34 +0800

    改用 HomeController回傳

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('home',function(){
-    return view('home.index');
-});
+ Route::get('home',[\App\Http\Controllers\HomeController::class,'index']);
